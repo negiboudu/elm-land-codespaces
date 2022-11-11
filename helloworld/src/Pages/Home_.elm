a2cdb0c1b077d5a241912c563b90d48561c6e9fe
Katsuaki Suzuki <2309728+negiboudu@users.noreply.github.com>
2022-11-11 22:31:32 +0000

    first commit

---
+++ helloworld/src/Pages/Home_.elm
@@ -0,0 +1,14 @@
+module Pages.Home_ exposing (page)
+
+import Html
+import View exposing (View)
+
+
+page : View msg
+page =
+    { title = "Homepage"
+    , body =
+        [ Html.text "Hello, world!"
+        , Html.h1 [] [ Html.text "Elm Landでつくりました" ]
+        ]
+    }
